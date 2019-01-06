c0bc70905545adc555a541b93d79ec5d2be48960
Dominik García <dominikgbertapelle@gmail.com>
2019-01-05 21:48:39 -0600

    alert link tests and refactor

--- src/Alert.js
+++ src/Alert.js
@@ -9,10 +9,39 @@ import colors from './styles/alert';
 
 type Props = {
   element: string,
+  children: any,
+  type: string,
 };
 
 const createComponent = (StyledComponent, props) =>
   React.createElement(StyledComponent, props);
+
+const buildAlertLink = (props: Props) => {
+  const StyledAlertLink = styled(props.element)`
+    font-weight: 700;
+    color: ${styledProps(colors.link, 'type')};
+  `;
+  return createComponent(StyledAlertLink, props);
+};
+
+const AlertLink = (props: Props) => buildAlertLink(props);
+
+AlertLink.defaultProps = {
+  element: 'a',
+  type: 'primary',
+};
+
+const searchForAlertLink = (children, type) =>
+  React.Children.map(children, child => {
+    if (!child.props || !child.props.children) return child;
+    if (child.type === AlertLink) {
+      return React.cloneElement(child, {
+        children: searchForAlertLink(child.props.children, type),
+        type,
+      });
+    }
+    return child;
+  });
 
 const buildAlert = (props: Props) => {
   const StyledAlert = styled(props.element)`
@@ -25,28 +54,17 @@ const buildAlert = (props: Props) => {
     background: ${styledProps(colors.backgrounds, 'type')};
     color: ${styledProps(colors.text, 'type')};
   `;
-  return createComponent(StyledAlert, props);
-};
-
-const buildAlertLink = (props: Props) => {
-  const StyledAlertLink = styled(props.element)`
-    font-weight: 700;
-    color: ${styledProps(colors.link, 'type')};
-  `;
-  return createComponent(StyledAlertLink, props);
+  let { children } = props;
+  if (children) {
+    children = searchForAlertLink(children, props.type);
+  }
+  return createComponent(StyledAlert, { ...props, children });
 };
 
 const Alert = (props: Props) => buildAlert(props);
 
 Alert.defaultProps = {
   element: 'div',
-  type: 'primary',
-};
-
-const AlertLink = (props: Props) => buildAlertLink(props);
-
-AlertLink.defaultProps = {
-  element: 'a',
   type: 'primary',
 };
 
