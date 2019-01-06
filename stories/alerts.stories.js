844a52d0d65cbb9124de2decd03db4c0d0eb642c
Dominik García <dominikgbertapelle@gmail.com>
2019-01-06 16:12:25 -0600

    added travis.yml and bundlesize

--- stories/alerts.stories.js
+++ stories/alerts.stories.js
@@ -3,46 +3,60 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 
-import { Alert, BootstrapWrapper } from '../src';
+import { Alert, AlertLink, BootstrapWrapper } from '../src';
 
 storiesOf('Alerts', module)
-  .add('Primary', () => (
+  .add('Simple', () => (
     <BootstrapWrapper>
       <Alert>A simple primary alert—check it out!</Alert>
-    </BootstrapWrapper>
-  ))
-  .add('Secondary', () => (
-    <BootstrapWrapper>
       <Alert type="secondary">A simple secondary alert—check it out!</Alert>
-    </BootstrapWrapper>
-  ))
-  .add('Success', () => (
-    <BootstrapWrapper>
       <Alert type="success">A simple success alert—check it out!</Alert>
-    </BootstrapWrapper>
-  ))
-  .add('Danger', () => (
-    <BootstrapWrapper>
       <Alert type="danger">A simple danger alert—check it out!</Alert>
-    </BootstrapWrapper>
-  ))
-  .add('Warning', () => (
-    <BootstrapWrapper>
       <Alert type="warning">A simple warning alert—check it out!</Alert>
-    </BootstrapWrapper>
-  ))
-  .add('Info', () => (
-    <BootstrapWrapper>
       <Alert type="info">A simple info alert—check it out!</Alert>
-    </BootstrapWrapper>
-  ))
-  .add('Light', () => (
-    <BootstrapWrapper>
       <Alert type="light">A simple light alert—check it out!</Alert>
+      <Alert type="dark">A simple dark alert—check it out!</Alert>
     </BootstrapWrapper>
   ))
-  .add('Dark', () => (
+  .add('With link', () => (
     <BootstrapWrapper>
-      <Alert type="dark">A simple dark alert—check it out!</Alert>
+      <Alert>
+        A simple primary alert with{' '}
+        <AlertLink href="#">an example link</AlertLink>. Give it a click if you
+        like.
+      </Alert>
+      <Alert type="secondary">
+        A simple secondary alert with{' '}
+        <AlertLink href="#">an example link</AlertLink>. Give it a click if you
+        like.
+      </Alert>
+      <Alert type="success">
+        A simple success alert with{' '}
+        <AlertLink href="#">an example link</AlertLink>. Give it a click if you
+        like.
+      </Alert>
+      <Alert type="danger">
+        A simple danger alert with{' '}
+        <AlertLink href="#">an example link</AlertLink>. Give it a click if you
+        like.
+      </Alert>
+      <Alert type="warning">
+        A simple warning alert with{' '}
+        <AlertLink href="#">an example link</AlertLink>. Give it a click if you
+        like.
+      </Alert>
+      <Alert type="info">
+        A simple info alert with <AlertLink href="#">an example link</AlertLink>
+        . Give it a click if you like.
+      </Alert>
+      <Alert type="light">
+        A simple light alert with{' '}
+        <AlertLink href="#">an example link</AlertLink>. Give it a click if you
+        like.
+      </Alert>
+      <Alert type="dark">
+        A simple dark alert with <AlertLink href="#">an example link</AlertLink>
+        . Give it a click if you like.
+      </Alert>
     </BootstrapWrapper>
   ));
